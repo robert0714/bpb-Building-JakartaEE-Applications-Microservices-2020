cb726e91758d1855bc47287943e0328432bb1f72
avbravo <avbravo@gmail.com>
2019-12-15 23:31:25 -0500

    eclipsekrasos

---
+++ chapter09/myk/src/main/java/com/avbravo/controller/repository/Greeting.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.controller.repository;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author avbravo
+ */
+@Named("greeting")
+@RequestScoped
+public class Greeting {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
